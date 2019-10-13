fb0abc3c8403434b907de750be5c694b51e13512
Vitaly Raicheu <RVitaly1978@gmail.com>
2019-10-13 11:11:56 +0200

    feat: implement class and pass all tests

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min_value = Math.ceil(min);
+        this.max_value = Math.floor(max);
     }
 
     guess() {
-
+        this.middle_value = Math.ceil(this.min_value + (this.max_value - this.min_value)/2);
+        return this.middle_value;
     }
 
     lower() {
-
+        this.max_value = this.middle_value;
     }
 
     greater() {
-
+        this.min_value = this.middle_value;
     }
 }
 
